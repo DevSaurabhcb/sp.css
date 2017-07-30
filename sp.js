f6f78b97eebd8a29d7e24b36da8c6a28ff4b257c
Saurabh Sharma <30465670+ss-pm@users.noreply.github.com>
2017-07-30 14:56:45 +0530

    updated sp.css and sp.js

--- sp.js
+++ sp.js
@@ -1,9 +1,12 @@
 window.onload = function(){
+//to ripple
 var toRipple = document.getElementsByClassName("sp-ripple");
 Array.prototype.forEach.call(toRipple,function(r){
 r.addEventListener("click",rippleIt);
 });
+}
 
+//functions here
 function rippleIt(e){
 var circle = document.createElement('div');
 this.appendChild(circle);
@@ -15,4 +18,27 @@ circle.style.left = e.clientX - this.offsetLeft - d/2+"px";
 circle.style.top = e.clientY - this.offsetTop - d/2+"px";
 circle.classList.add("ripple");
 }
+var sp = {
+
+createToast : function(text,duration){
+var toast = document.createElement('div');
+this.text = text ;
+this.duration = duration;
+this.duration = (typeof this.duration !== 'undefined') ?  this.duration : 2;
+toast.style.height = "30px";
+toast.style.backgroundColor = "rgba(0,0,0,0.5)";
+toast.style.color = "#fff";
+toast.style.padding = "5px 10px";
+toast.style.position= "fixed";
+toast.style.zIndex = "999";
+toast.style.bottom = "50px";
+toast.style.right = "50%";
+toast.style.transform = "translateX(50%)";
+toast.style.borderRadius = "10px";
+toast.innerHTML = this.text;
+document.body.appendChild(toast);
+setTimeout(function(){
+document.body.removeChild(toast);
+},this.duration*1000);
+},
 }
