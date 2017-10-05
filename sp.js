8fa0a7e30b62bf7f6640ec2882a053efd19e59b8
Saurabh Sharma <30465670+DevSaurabhcb@users.noreply.github.com>
2017-10-05 18:05:22 +0530

    Add files via upload

--- sp.js
+++ sp.js
@@ -1,51 +1,228 @@
-window.onload = function(){
-//to ripple
-var toRipple = document.getElementsByClassName("sp-ripple");
-Array.prototype.forEach.call(toRipple,function(r){
-r.addEventListener("click",rippleIt);
-});
-var materialnavopener_ = document.getElementsByClassName("sp-material-nav-opener");
-Array.prototype.forEach.call(materialnavopener_,function(r){
-r.addEventListener("click",materialnav);
-});
-if(window.innerWidth<480){
-var materialnav_ = document.getElementsByClassName("sp-material-nav");
-Array.prototype.forEach.call(materialnav_,function(nav){
-/*creating a back button to close the material nav*/
-var closemtrlnvbtn = document.createElement('div');
-nav.appendChild(closemtrlnvbtn);
-/* for modern browsers
-closemtrlnvbtn.style.display = "flex";
-closemtrlnvbtn.style.alignItems = "center";
-closemtrlnvbtn.style.justifyContent = "center";
-*/
-closemtrlnvbtn.style.textAlign = "center";
-closemtrlnvbtn.style.padding = "0";
-closemtrlnvbtn.style.position = "absolute";
-closemtrlnvbtn.style.left = "10px";
-closemtrlnvbtn.style.top = "10px";
-closemtrlnvbtn.style.width = "30px";
-closemtrlnvbtn.style.height = "30px";
-closemtrlnvbtn.style.lineHeight = "29px";
-closemtrlnvbtn.classList.add("text-white","sp-black","sp-shadow_3","sp-circle","font-2x");
-closemtrlnvbtn.innerHTML = "&larr;";
-closemtrlnvbtn.addEventListener("click",function(){nav.style.left="-290px";});
-});
+(function (root, factory) {
+  
+  "use strict";
+  
+  if ( typeof module === "object" && typeof module.exports === "object" ) {
+  
+  module.exports = root.document ?
+  factory( root, true ) :
+  function( w ) {
+  if ( !w.document ) {
+  throw new Error( "Required a window with a document" );
+  }
+  return factory( w );
+  };
+  } else {
+  factory( root );
+  }
+}(this, function (w) {
+  
+  
+  var SP = function(selector){
+  var self = {};
+  self.selector = selector;
+  self.elements = document.querySelectorAll(selector);
+  /*max z value*/
+  self.maxZ = function(){
+  var z = null,
+  eles = document.querySelectorAll("*");
+  for(var x=0;x<=eles.length;x++){
+  if(typeof eles[x] !== "undefined"){
+  if(eles[x].style.zIndex){
+  if(eles[x].style.zIndex>z){
+  z=eles[x].style.zIndex;
+  }
+  }
+  }
+  }
+  return z ;
+  return self;
+  }
+  /*max z end*/
+  /*sp toast*/
+  self.createToast = function(text,duration){
+  var toast = document.createElement('div');
+  this.text = text ;
+  this.duration = duration;
+  this.duration = (typeof this.duration !== 'undefined') ?  this.duration : 1.5;
+  toast.classList.add("sp-toast");
+  toast.innerHTML = this.text;
+  document.body.appendChild(toast);
+  setTimeout(function(){
+  document.body.removeChild(toast);
+  },this.duration*1000);
+  return self;
+  }
+  /*End sp toast*/
+  /*events*/
+  self.click = function(callback){
+  Array.prototype.forEach.call(self.elements,function(r){
+  r.addEventListener('click',callback);
+  });
+  return self;
+  }
+  self.dblclick = function(callback){
+  $(self.selector).on('dblclick',callback);
+  return self;
+  }
+  self.on = function(e,callback){
+  switch(e) {
+  case "swipeleft":
+  $(self.selector).swipe("left",callback);
+  break;
+  case "swiperight":
+  $(self.selector).swipe("right",callback);
+  break;
+  case "swipedown":
+  $(self.selector).swipe("down",callback);
+  break;
+  case "swipeup":
+  $(self.selector).swipe("up",callback);
+  break;
+  default:
+  Array.prototype.forEach.call(self.elements,function(r){
+  r.addEventListener(e,callback);
+  });
+  }
+  return self;
+  }
+  /*now swipe function*/
+  self.swipe = function(dir,callback){
+  Array.prototype.forEach.call(self.elements,function(r){
+  r.addEventListener('touchstart',handleStart,false);
+  r.addEventListener('touchmove',handleMove,false);
+  r.addEventListener('touchend',handleEnd,false)
+  });
+  
+  var xi = null,
+  yi = null,
+  xf = null,
+  yf = null,
+  opt,
+  minArea = 150,
+  direction;
+ 
+  function handleStart(e){
+  xi = e.touches[0].screenX;
+  yi = e.touches[0].screenY;
+  }
+  function handleMove(e){
+  xf = e.touches[0].screenX;
+  yf = e.touches[0].screenY;
+  }
+  function handleEnd(e){
+  x = xf-xi;
+  y = yf-yi;
+  if(Math.abs(x)>minArea||Math.abs(y)>minArea){
+  if(Math.abs(x)>Math.abs(y)){
+  if(xf>xi){
+  direction="right";
+  }
+  else{
+  direction="left";
+  }
+  }
+  else{
+  if(yf>yi){
+  direction="down";
+  }
+  else{
+  direction="up";
+  }
+  }
+  }
+  else{
+  direction = undefined;
+  }
+  if(dir==direction){
+  callback();
+  }
+  }
+  return self;
+  }
+  /*End of swipe*/
+  /*CSS*/
+  self.css = function(o){
+  if(typeof o === "object"){
+  Array.prototype.forEach.call(self.elements,function(r){
+  for(var prop in o){
+  var p = prop.replace(/-([a-z])/g, function (m, w) {
+  return w.toUpperCase();
+  });
+  if(o.hasOwnProperty(prop)){
+  r.style[p] = o[prop];
+  }
+  }
+  });
+  }
+  else{
+  console.log(typeof o + " is not an Object , can't set css");
+  }
+  return self;
+  }
+  /*End of CSS*/
+  
+  return self;
+  }
+  
+  
+SP.noConflict = function( c ) {
+	if ( window.$ === SP ) {
+		window.$ = _$;
+	}
+
+	if ( c && window.SP === SP ) {
+		window.SP = _SP;
+	}
+
+	return SP;
+};
+ 
+if ( typeof define === "function" && define.amd ) {
+	define( "SP", [], function() {
+		return SP;
+	} );
 }
+window.SP = window.$ = SP;
+
+return SP;
+}));
+
+
+
+window.onload = function(){
+_sp_init();
+//to ripple
+var _toRipple = document.getElementsByClassName("sp-ripple");
+Array.prototype.forEach.call(_toRipple,function(r){
+r.addEventListener("click",_sp_rippleIt);
+});
+var _toRipple_dark = document.getElementsByClassName("sp-ripple_dark");
+Array.prototype.forEach.call(_toRipple_dark,function(r){
+r.addEventListener("click",_sp_rippleIt_dark);
+});
+var _material_nav_opener_ = document.getElementsByClassName("sp-material-nav_opener");
+Array.prototype.forEach.call(_material_nav_opener_,function(r){
+r.addEventListener("click",_sp_materialnav);
+});
 
 }
+function _sp_init(){
+if(document.querySelector('.sp-header')!=null){
+document.querySelector('.sp-container').style.paddingTop="55px";
+}
+var ct = document.querySelector(".sp-container");
+if(window.innerWidth>=900){
+if(document.querySelector(".sp-material-nav.autoopen")!=null){
+ct.style.marginLeft = "280px";
+}
+}
+}
 /*material nav opener*/
-function materialnav(){
+function _sp_materialnav(){
 document.getElementById(this.getAttribute("data-target")).style.left="0";
 var navcover = document.createElement('div');
-navcover.style.width="100%";
-navcover.style.height="100%";
-navcover.style.left="0";
-navcover.style.top="0";
-navcover.style.position="fixed";
-navcover.style.zIndex="998";
-navcover.style.transition="0.4s linear opacity";
-navcover.style.backgroundColor="rgba(0,0,0,.4)";
+navcover.classList.add("sp-navcover_mat");
 navcover.addEventListener("click",function(){
 document.querySelector(".sp-material-nav").style.left="-290px";
 document.body.removeChild(navcover);
@@ -53,7 +230,7 @@ document.body.removeChild(navcover);
 
 document.body.appendChild(navcover);
 
-/*adding swipe left to nav*/
+/*adding swipe left to mat nav*/
 $(".sp-material-nav").swipe("left",function(){
 document.querySelector(".sp-material-nav").style.left="-290px";
 document.body.removeChild(navcover);
@@ -61,103 +238,48 @@ document.body.removeChild(navcover);
 
 }
 /*sp ripple function*/
-function rippleIt(e){
+function _sp_rippleIt(e){
 var circle = document.createElement('div');
 this.appendChild(circle);
 
 var d = Math.max(this.clientWidth,this.clientHeight);
 circle.style.width = circle.style.height = d+"px";
-if(this.style.background=="#fff"||this.style.background=="white"||this.style.background=="transparent"||this.style.backgroundColor=="#fff"||this.style.backgroundColor=="white"||this.style.backgroundColor=="transparent"){
-circle.style.backgroundColor="rgba(0,0,0,0.3)";
+circle.classList.add("ripple");
+
+circle.style.backgroundColor="rgba(255,255,255,0.9)";
+
+var style = window.getComputedStyle(this);
+var position = style.getPropertyValue("position");
+
+if(position !="fixed"){
+circle.style.left = e.pageX - this.offsetLeft - d/2+"px";
+circle.style.top = e.pageY - this.offsetTop - d/2+"px";
 }
 else{
-circle.style.backgroundColor="rgba(255,255,255,0.7)";
+circle.style.left = e.screenX - this.offsetLeft - d/2+"px";
+circle.style.top = e.screenY - this.offsetTop - d/2+"px";
 }
-circle.style.left = e.clientX - this.offsetLeft - d/2+"px";
-circle.style.top = e.clientY - this.offsetTop - d/2+"px";
+setTimeout(function(){circle.style.display="none";},600);
+}
+function _sp_rippleIt_dark(e){
+var circle = document.createElement('div');
+this.appendChild(circle);
+
+var d = Math.max(this.clientWidth,this.clientHeight);
+circle.style.width = circle.style.height = d+"px";
 circle.classList.add("ripple");
-setTimeout(function(){circle.style.display="none";},500);
+
+circle.style.backgroundColor="rgba(0,0,0,0.7)";
+var style = window.getComputedStyle(this);
+var position = style.getPropertyValue("position");
+
+if(position !="fixed"){
+circle.style.left = e.pageX - this.offsetLeft - d/2+"px";
+circle.style.top = e.pageY - this.offsetTop - d/2+"px";
 }
-
-
-/*sp function is here as $()*/
-function $(selector){
-var self = {};
-self.selector = selector;
-self.element = document.querySelector(selector);
-
-self.createToast = function(text,duration){
-var toast = document.createElement('div');
-this.text = text ;
-this.duration = duration;
-this.duration = (typeof this.duration !== 'undefined') ?  this.duration : 1.5;
-toast.style.minHeight = "30px";
-toast.style.fontSize = "16px";
-toast.style.maxWidth = "95%";
-toast.style.wordWrap = "break-word";
-toast.style.backgroundColor = "rgba(0,0,0,0.7)";
-toast.style.color = "#fff";
-toast.style.padding = "10px 20px";
-toast.style.position= "fixed";
-toast.style.zIndex = "999";
-toast.style.bottom = "20px";
-toast.style.right = "50%";
-toast.style.transform = "translateX(50%)";
-toast.style.borderRadius = "20px";
-toast.innerHTML = this.text;
-document.body.appendChild(toast);
-setTimeout(function(){
-document.body.removeChild(toast);
-},this.duration*1000);
+else{
+circle.style.left = e.screenX - this.offsetLeft - d/2+"px";
+circle.style.top = e.screenY - this.offsetTop - d/2+"px";
 }
-/*now swipe function*/
-self.swipe = function(dir,callback){
-self.element.addEventListener('touchstart',handleStart,false);
-self.element.addEventListener('touchmove',handleMove,false);
-
-var xi = null;
-var yi = null;
-var xf = null;
-var yf = null;
-var direction;
-function handleStart(e){
-xi = e.touches[0].clientX;
-yi = e.touches[0].clientY;
-}
-function handleMove(e){
-if(!xi || !yi){return;}
-
-xf = e.touches[0].clientX;
-yf = e.touches[0].clientY;
-
-var x = xi-xf;
-var y = yi-yf;
-if ( Math.abs(x) > Math.abs(y) ) {/*most significant*/
-        if ( x > 0 ) {
-            /* left swipe */ 
-            direction="left";
-        } else {
-            /* right swipe */
-            direction="right";
-        }                       
-    } else {
-        if ( y > 0 ) {
-            /* up swipe */
-            direction = "up";
-        } else { 
-            /* down swipe */
-            direction = "down";
-        }                                                                 
-    }
-    /* reset values */
-    xi = null;
-    yi = null;
-if(dir==direction){
-callback();
-console.log(x+" "+y);
-}
-}
-}
-
-return self;
+setTimeout(function(){circle.style.display="none";},600);
 }
